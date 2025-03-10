e30721fff3552f6570f8bdcf187108520a8e1e91
feltcute <feltcute@users.noreply.github.com>
2025-03-10 14:11:39 -0500

    that number is over here too, probably can be simplified into a var that updates from just one spot

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@ needs_sphinx = '7.2.6'
 project = "Monster Girl Dreams"
 copyright = '2017-2024, Threshold'
 author = 'Threshold'
-release = 'v26.3'
+release = 'v26.8'
 
 # -- General configuration ---------------------------------------------------
 
